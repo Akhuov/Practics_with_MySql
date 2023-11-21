04db703171ed0b81932dab90d520a46d951b6d42
user <user@WIN-F7NIMF7A3VO>
2023-11-21 07:39:13 +0500

    Добавьте файлы проекта.

---
+++ Practics_with_stream/Program.cs
@@ -0,0 +1,31 @@
+﻿using MySql.Data.MySqlClient;
+using Practics_with_stream;
+using System.Data;
+
+String Id = Console.ReadLine();
+int Pass = int.Parse(Console.ReadLine());
+
+DB dB = new DB();
+
+var table = new DataTable();
+
+MySqlDataAdapter adapter = new MySqlDataAdapter();
+
+MySqlCommand command = new MySqlCommand("SELECT * FROM 'gr' WHERE 'member' = Id AND 'card' = Pass", dB.getConnection());
+
+//command.Parameters.Add("@uI", MySqlDbType.VarChar).Value = Id;
+//command.Parameters.Add("@uP", MySqlDbType.Int32).Value = Pass;
+
+adapter.SelectCommand = command;
+adapter.Fill(table);
+
+if (table.Rows.Count > 0)
+{
+    Console.WriteLine(table);
+}
+
+else
+{
+
+    Console.WriteLine("No");
+}
---
+++ Practics_with_stream/ServisClass.cs
@@ -0,0 +1,30 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace Practics_with_stream
+{
+
+    internal class DB
+    {
+        MySqlConnection connection = new MySqlConnection("server = localhost; port = 3306;username = root;password = password;database = foundation");
+
+        public void openConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+                connection.Open();
+        }
+
+        public void closeConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Open)
+                connection.Close();
+        }
+
+        public MySqlConnection getConnection()
+        {
+            return connection;
+        }
+
+
+    }
+}
